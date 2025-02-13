941625e754ea3b729298bb3f075ed24b043dad91
Balla Botond <ballabtnd@gmail.com>
2025-02-13 12:53:13 +0100

    Create a full django project of a working blog

---
+++ blog/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Post, Image
+
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Image)
